c7337a7996091f05d638b2047fb7c4537448db2f
rafaelsmartphone@gmail.com <rafaelsmartphone@gmail.com>
2024-07-03 14:41:13 -0500

    Finish date class new

---
+++ concentpsPOO/concentpsPOO/Date.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace concentpsPOO
+{
+    public class Date
+    {
+        private int _year;
+        private int _month;
+        private int _day;
+      
+        public Date(int year, int month, int day)
+
+        {
+            _year = year;
+            _month = CheckMonth(month);
+            _day = CheckDay(year, month, day);         
+      
+        }
+
+        private int CheckDay(int year, int month, int day)
+        {
+            // para validad cuando el año sea viciesto
+
+            if (month == 2 && day == 29 && IsLeapYear(year))
+            {
+                return day;
+            }
+            // validad que los meses 
+            int[] daysPerMont = { 0, 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31 };
+            if (day >= 1 && day <= daysPerMont[month])
+            {
+                return day;
+            }
+            throw new DayException("Invalid Day");
+        }
+
+        private bool IsLeapYear(int year)
+        {
+            return year % 400 == 0 || year % 4 == 0 && year % 100 != 0;
+            //METODO MAS LARGO PARA VALIDAR SI EL AÑO ES BICIESTO
+            //if (year % 4 == 0)
+            //{
+            //    if (year % 100 == 0)
+            //    {
+            //        if (year % 400 == 0)
+            //        { 
+            //            return true;
+            //        }
+            //        else
+            //        {
+            //            return false;
+            //        }
+            //    }
+            //    else
+            //    {
+            //        return true; 
+            //    }
+            //}
+            //else
+            //{
+            //    return false;
+            //}
+
+        }
+        //Validad que el mes no tenga mas de 12 
+        private int CheckMonth(int month)
+        {
+            if (month >= 1 && month <= 12)
+
+            {
+                return month;
+            }
+                throw new MonthException("Invalid month");
+        }
+
+        public override string ToString()
+        {
+            return $"{_year}/{_month:00}/{_day:00}";
+                
+        }
+        
+           
+        }
+    }
+
+
---
+++ concentpsPOO/concentpsPOO/Program.cs
@@ -0,0 +1,16 @@
+﻿// See https://aka.ms/new-console-template for more information
+using concentpsPOO;
+
+Console.WriteLine("HOLA PICHE DE MUNDO ");
+Console.WriteLine("-------------------");
+try
+{
+	Console.WriteLine(new Date(2024, 2, 29));
+	Console.WriteLine(new Date(2024, 07, 21));
+	Console.WriteLine(new Date(2024, 11, 31));
+}
+catch (Exception error)
+{
+
+	Console.WriteLine(error.Message);
+}
